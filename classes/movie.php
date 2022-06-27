d2a36c136fc981edb37b87051a9409d58a504002
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-27 16:08:37 +0200

    creata classe 'movie' con dichiarazione viariabili e costrutto

---
+++ classes/movie.php
@@ -0,0 +1,23 @@
+ 
+ <?php 
+
+ class Moovie{
+    public $title;
+    public $author;
+    public $genre;
+    public $year;
+    public $cast;
+
+    public function __construct($_title, $_author, $_genre, $_year)
+    {
+        $this-> title = $_title;
+        $this-> title = $_author;
+        $this-> title = $_genre;
+        $this-> title = $_year;
+        
+    }
+
+ }
+ 
+ 
+ ?>
