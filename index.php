91b0cc9abc1e57e405694f058185272a1c33e394
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-27 16:18:32 +0200

    aggiunto valori proprieta film

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php ?>
+<?php 
+    require_once  __DIR__ .'/classes/movie.php';
+
+    $movies = [
+        new movie('Fast And Furios', 'Gary Scott Thompson', 'avventura', 2001),
+        new movie('Il padrino', 'Francis Ford Coppola', 'crime', 1972),
+        new movie('Inception', 'Christopher Nolan', 'avventura', 2010) ,
+        new movie('Inception', 'Christopher Nolan', 'avventura', 2010),
+        new movie('Jurassic World ', 'Colin Trevorrow', 'avventura', 2022),
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
